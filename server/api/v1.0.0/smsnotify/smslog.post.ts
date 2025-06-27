9339a2531f267f0715ede7525ac048aecfba387f
Teerin Leepaiboon <l.teerin@gmail.com>
2025-06-27 22:59:38 +0700

    modify smslog api

--- server/api/v1.0.0/smsnotify/smslog.post.ts
+++ server/api/v1.0.0/smsnotify/smslog.post.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import Debug from "debug";
+import { errorMessages } from "vue/compiler-sfc";
 import { validateNewPartner } from "~/models/cyberpay";
+import { SmsLog } from "~/types/smslog";
 
 const prisma = new PrismaClient();
 const debug = Debug("api:smsnotify:smslog");
@@ -8,15 +10,64 @@ const debug = Debug("api:smsnotify:smslog");
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  console.log(body.msg);
-  const res = {
-    status: true,
-    message: "success",
-    data: {
-      // transaction_id:body.transaction_id
-      msg: body.msg,
-    },
-  };
+  const data: SmsLog = body.msg;
+  console.log("Data: ", data);
 
-  return res;
+  const text = data.message;
+
+  // ดึง deposit to
+  const depositToMatch = text.match(/Deposit to (\w+)/);
+  const amountMatch = text.match(/amount\s([\d,.]+)\sBaht/);
+  const balanceMatch = text.match(/balance\s([\d,.]+)\sBaht/);
+
+  // แปลงค่าที่ได้
+  const depositTo = depositToMatch ? depositToMatch[1] : null;
+  const amount = amountMatch
+    ? parseFloat(amountMatch[1].replace(/,/g, ""))
+    : null;
+
+  const balance = balanceMatch
+    ? parseFloat(balanceMatch[1].replace(/,/g, ""))
+    : null;
+
+  //   console.log("Deposit To:", depositTo); // xxx122856x
+  //   console.log("Amount:", amount); // 2.01
+  //   console.log("Balance:", balance); // 38826.03
+
+  data.bankAccount = depositTo as string;
+  data.amount = amount;
+  data.balance = balance;
+
+  // แปลงเป็น Date โดยใช้ moment หรือ JavaScript ล้วน
+
+  const parsedTime = new Date(
+    data.time.replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$3-$2-$1")
+  ); // → "2025-06-27T21:09:00.000Z"
+
+  if (isNaN(parsedTime.getTime())) {
+    throw new Error("Invalid time format");
+  }
+
+  try {
+    const saveData = await prisma.smslog.create({
+      data: {
+        ...data,
+        time: parsedTime,
+      },
+    });
+
+    const res = {
+      status: true,
+      message: "success",
+      data: saveData,
+    };
+
+    return res;
+  } catch (err) {
+    console.error("Prisma Error:", err);
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Failed to save",
+    });
+  }
 });
---
+++ types/smslog.d.ts
@@ -0,0 +1,11 @@
+export interface SmsLog {
+  id: string
+  shop: string
+  sender: string
+  time: string
+  message: string
+  bankAccount:         string
+  amount:              float
+  balance:             float
+}
+
