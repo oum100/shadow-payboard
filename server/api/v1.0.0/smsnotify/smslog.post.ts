11d3176ea2ea0b0da2a1f0aa474ff9ac99b53819
Teerin Leepaiboon <l.teerin@gmail.com>
2025-06-27 21:24:24 +0700

    modify smslog api

--- server/api/v1.0.0/smsnotify/smslog.post.ts
+++ server/api/v1.0.0/smsnotify/smslog.post.ts
@@ -1,24 +1,22 @@
-
 import { PrismaClient } from "@prisma/client";
-import Debug from 'debug'
-import {validateNewPartner} from '~/models/cyberpay'
-
+import Debug from "debug";
+import { validateNewPartner } from "~/models/cyberpay";
 
 const prisma = new PrismaClient();
-const debug = Debug('api:smsnotify:smslog');
+const debug = Debug("api:smsnotify:smslog");
 
-export default defineEventHandler( async(event)=>{
-    const body = await readBody(event)
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
 
-    console.log(body)
-    const res = {
-        status:true,
-        message:'success',
-        data:{
-            // transaction_id:body.transaction_id
-            msg:"ok"
-        }
-    }
+  console.log(body.msg);
+  const res = {
+    status: true,
+    message: "success",
+    data: {
+      // transaction_id:body.transaction_id
+      msg: body.msg,
+    },
+  };
 
-    return res
-} )
+  return res;
+});
